1870a89e3a51baf3ed80b4d54e4ff1e00f6768b8
Thomas Kastner <t.kastner@vace.at>
2018-05-09 13:12:45 +0200

    First couple of functions and structures

---
+++ solarwinds.go
@@ -0,0 +1,120 @@
+package solarwinds
+
+import (
+	"encoding/xml"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"golang.org/x/net/html/charset"
+)
+
+// Solarwinds is a client for the data extraction API of Solarwinds RMM.
+type Solarwinds struct {
+	Host string
+	Key  string
+}
+
+func (api Solarwinds) get(values url.Values, dst interface{}) error {
+	values.Set("apikey", api.Key)
+	apiurl := (&url.URL{
+		Scheme:   "https",
+		Host:     api.Host,
+		Path:     "/api/",
+		RawQuery: values.Encode(),
+	}).String()
+
+	resp, err := http.Get(apiurl)
+	if err != nil {
+		return err
+	}
+
+	dec := xml.NewDecoder(resp.Body)
+	dec.CharsetReader = charset.NewReaderLabel
+	err = dec.Decode(&dst)
+	return err
+}
+
+// ListClients returns all clients.
+func (api Solarwinds) ListClients() ([]Client, error) {
+	body := struct {
+		Items []Client `xml:"items>client"`
+	}{}
+
+	err := api.get(url.Values{
+		"service": []string{"list_clients"},
+	}, &body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body.Items, nil
+}
+
+// ListSites retuns all sites for a client.
+func (api Solarwinds) ListSites(clientid int) ([]Site, error) {
+	body := struct {
+		Items []Site `xml:"items>site"`
+	}{}
+
+	err := api.get(url.Values{
+		"service":  []string{"list_sites"},
+		"clientid": []string{strconv.Itoa(clientid)},
+	}, &body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body.Items, nil
+}
+
+// ListServers returns all server monitoring devices for a site (including top level asset information if available).
+func (api Solarwinds) ListServers(siteid int) ([]Server, error) {
+	body := struct {
+		Items []Server `xml:"items>server"`
+	}{}
+
+	err := api.get(url.Values{
+		"service": []string{"list_servers"},
+		"siteid":  []string{strconv.Itoa(siteid)},
+	}, &body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body.Items, nil
+}
+
+// ListWorkstations returns all workstation monitoring devices for a site (including top level asset information if available).
+func (api Solarwinds) ListWorkstations(siteid int) ([]Workstation, error) {
+	body := struct {
+		Items []Workstation `xml:"items>workstation"`
+	}{}
+
+	err := api.get(url.Values{
+		"service": []string{"list_workstations"},
+		"siteid":  []string{strconv.Itoa(siteid)},
+	}, &body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body.Items, nil
+}
+
+// ListDeviceSoftware returns a list of installed software for a device.
+func (api Solarwinds) ListDeviceSoftware(deviceid int) ([]Software, error) {
+	body := struct {
+		Software []Software `xml:"software>item"`
+	}{}
+
+	err := api.get(url.Values{
+		"service":  []string{"list_device_asset_details"},
+		"deviceid": []string{strconv.Itoa(deviceid)},
+	}, &body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body.Software, nil
+}
---
+++ structs.go
@@ -0,0 +1,171 @@
+package solarwinds
+
+// Client describes a customer
+type Client struct {
+	ID                    int    `xml:"clientid"`
+	Name                  string `xml:"name"`
+	ViewDashboard         int    `xml:"view_dashboard"`
+	ViewWorkstationAssets int    `xml:"view_wkstsn_assets"`
+	DashboardUsername     string `xml:"dashboard_username"`
+	Timezone              string `xml:"timezone"`
+	CreationDate          string `xml:"creation_date"`
+	ServerCount           int    `xml:"server_count"`
+	WorkstationCount      int    `xml:"workstation_count"`
+	MobileDeviceCount     int    `xml:"mobile_device_count"`
+	DeviceCount           int    `xml:"device_count"`
+}
+
+// Site describes the different locations of a client
+type Site struct {
+	ID              int    `xml:"siteid"`
+	Name            string `xml:"name"`
+	PrimaryRouter   string `xml:"primary_router"`
+	SecondaryRouter string `xml:"secondary_router"`
+	ConnectionOK    string `xml:"connection_ok"`
+	CreationDate    string `xml:"creation_date"`
+}
+
+// Server describes a computer used as a server
+type Server struct {
+	ID                   int     `xml:"serverid"`
+	GUID                 string  `xml:"guid"`
+	Name                 string  `xml:"name"`
+	Description          string  `xml:"description"`
+	AgentVersion         string  `xml:"agent_version"`
+	InstallDate          string  `xml:"install_date"`
+	LastBootTime         int     `xml:"last_boot_time"`
+	Online               int     `xml:"online"`
+	Active24x7           int     `xml:"active_247"`
+	CheckInterval24x7    int     `xml:"check_interval_247"`
+	Status24x7           int     `xml:"status_247"`
+	LocalDate24x7        string  `xml:"local_date_247"`
+	LocalTime24x7        string  `xml:"local_time_247"`
+	UTCTime24x7          string  `xml:"utc_time_247"`
+	DscActive            int     `xml:"dsc_active"`
+	AgentMode            int     `xml:"agent_mode"`
+	DscHour              int     `xml:"dsc_hour"`
+	DscStatus            int     `xml:"dsc_status"`
+	DscLocalDate         string  `xml:"dsc_local_date"`
+	DscLocalTime         string  `xml:"dsc_local_time"`
+	DscUTCTime           string  `xml:"dsc_utc_time"`
+	MissFactor24x7       float64 `xml:"miss_factor_247"`
+	EMailOverdueAlert    int     `xml:"email_overdue_alert"`
+	SMSOverdueAlert      int     `xml:"sms_overdue_alert"`
+	Missed24x7           int     `xml:"missed_247"`
+	RemoteConnectionType int     `xml:"remote_connection_type"`
+	RemoteAddress        string  `xml:"remote_address"`
+	RemotePort           int     `xml:"remote_port"`
+	RemoteUsername       string  `xml:"remote_username"`
+	RemoteDomain         string  `xml:"remote_domain"`
+	TzBias               int     `xml:"tz_bias"`
+	TzDstBias            int     `xml:"tz_dst_bias"`
+	TzStdBias            int     `xml:"tz_std_bias"`
+	TzMode               int     `xml:"tz_mode"`
+	ATzDstDate           string  `xml:"atz_dst_date"`
+	TzDstDate            string  `xml:"tz_dst_date"`
+	TzStdDate            string  `xml:"tz_std_date"`
+	UTCApt               string  `xml:"utc_apt"`
+	UTCOffset            int     `xml:"utc_offset"`
+	AssetID              int     `xml:"assetid"`
+	WINSName             string  `xml:"wins_name"`
+	User                 string  `xml:"user"`
+	Domain               string  `xml:"domain"`
+	Role                 string  `xml:"role"`
+	ChassisType          int     `xml:"chassis_type"`
+	Manufacturer         string  `xml:"manufacturer"`
+	Model                string  `xml:"model"`
+	DeviceSerial         string  `xml:"device_serial"`
+	IP                   string  `xml:"ip"`
+	ExternalIP           string  `xml:"external_ip"`
+	Mac1                 string  `xml:"mac1"`
+	Mac2                 string  `xml:"mac2"`
+	Mac3                 string  `xml:"mac3"`
+	ProcessorCount       int     `xml:"processor_count"`
+	TotalMemory          int     `xml:"total_memory"`
+	OSType               int     `xml:"os_type"`
+	OS                   string  `xml:"os"`
+	ServicePack          string  `xml:"service_pack"`
+	OSSerialNumber       string  `xml:"os_serial_number"`
+	OSPRoductKey         string  `xml:"os_product_key"`
+	LastScanTime         string  `xml:"last_scan_time"`
+}
+
+// Workstation describes a computer used as a workstation
+type Workstation struct {
+	ID                   int     `xml:"workstationid"`
+	GUID                 string  `xml:"guid"`
+	Name                 string  `xml:"name"`
+	AgentVersion         string  `xml:"agent_version"`
+	InstallDate          string  `xml:"install_date"`
+	LastBootTime         int     `xml:"last_boot_time"`
+	Online               int     `xml:"online"`
+	Active24x7           int     `xml:"active_247"`
+	CheckInterval24x7    int     `xml:"check_interval_247"`
+	Status24x7           int     `xml:"status_247"`
+	LocalDate24x7        string  `xml:"local_date_247"`
+	LocalTime24x7        string  `xml:"local_time_247"`
+	UTCTime24x7          string  `xml:"utc_time_247"`
+	DscActive            int     `xml:"dsc_active"`
+	AgentMode            int     `xml:"agent_mode"`
+	DscHour              int     `xml:"dsc_hour"`
+	DscStatus            int     `xml:"dsc_status"`
+	DscLocalDate         string  `xml:"dsc_local_date"`
+	DscLocalTime         string  `xml:"dsc_local_time"`
+	DscUTCTime           string  `xml:"dsc_utc_time"`
+	MissFactor24x7       float64 `xml:"miss_factor_247"`
+	EMailOverdueAlert    int     `xml:"email_overdue_alert"`
+	SMSOverdueAlert      int     `xml:"sms_overdue_alert"`
+	Missed24x7           int     `xml:"missed_247"`
+	RemoteConnectionType int     `xml:"remote_connection_type"`
+	RemoteAddress        string  `xml:"remote_address"`
+	RemotePort           int     `xml:"remote_port"`
+	RemoteUsername       string  `xml:"remote_username"`
+	RemoteDomain         string  `xml:"remote_domain"`
+	TzBias               int     `xml:"tz_bias"`
+	TzDstBias            int     `xml:"tz_dst_bias"`
+	TzStdBias            int     `xml:"tz_std_bias"`
+	TzMode               int     `xml:"tz_mode"`
+	ATzDstDate           string  `xml:"atz_dst_date"`
+	TzDstDate            string  `xml:"tz_dst_date"`
+	TzStdDate            string  `xml:"tz_std_date"`
+	UTCApt               string  `xml:"utc_apt"`
+	UTCOffset            int     `xml:"utc_offset"`
+	AssetID              int     `xml:"assetid"`
+	WINSName             string  `xml:"wins_name"`
+	User                 string  `xml:"user"`
+	Domain               string  `xml:"domain"`
+	Role                 string  `xml:"role"`
+	ChassisType          int     `xml:"chassis_type"`
+	Manufacturer         string  `xml:"manufacturer"`
+	Model                string  `xml:"model"`
+	DeviceSerial         string  `xml:"device_serial"`
+	IP                   string  `xml:"ip"`
+	ExternalIP           string  `xml:"external_ip"`
+	Mac1                 string  `xml:"mac1"`
+	Mac2                 string  `xml:"mac2"`
+	Mac3                 string  `xml:"mac3"`
+	ProcessorCount       int     `xml:"processor_count"`
+	TotalMemory          int     `xml:"total_memory"`
+	OSType               int     `xml:"os_type"`
+	OS                   string  `xml:"os"`
+	ServicePack          string  `xml:"service_pack"`
+	OSSerialNumber       string  `xml:"os_serial_number"`
+	OSPRoductKey         string  `xml:"os_product_key"`
+	LastScanTime         string  `xml:"last_scan_time"`
+}
+
+// DeviceAssetDetails contains details about assets of a server or workstation
+type DeviceAssetDetails struct {
+	Software []Software `xml:"software>items"`
+}
+
+// Software describes installed software on a server or workstation
+type Software struct {
+	ID          int    `xml:"softwareid"`
+	CatalogID   int    `xml:"catalogid"`
+	Name        string `xml:"name"`
+	Version     string `xml:"version"`
+	InstallDate string `xml:"install_date"`
+	Deleted     int    `xml:"deleted"`
+	Modified    int    `xml:"modified"`
+}
